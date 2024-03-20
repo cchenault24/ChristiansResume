9af96567777a725e86ef33eff4ee31564bf685c4
Christian Chenault <chenaucp@amazon.com>
2024-03-20 11:31:32 -0400

    about me responsiveness

--- src/components/Summary.tsx
+++ src/components/Summary.tsx
@@ -1,16 +1,21 @@
 import '../App.css';
+import { useBreakpoints } from '../hooks/useBreakpoints';
 
 export default function Summary() {
+    const { isMobile, isTablet } = useBreakpoints();
+
+    const isSmall = isMobile || isTablet;
+    const alignment = isSmall ? 'text-center' : 'text-left';
     return (
-            <p className='max-w-3xl'>
-                Results-driven Front End Engineer with a strong passion for
-                creating visually appealing and highly responsive web
-                applications. Leveraging expertise in modern web technologies, I
-                aim to contribute to a dynamic team where I can design and
-                develop user-friendly interfaces, optimize website performance,
-                and ensure seamless cross-browser compatibility. My goal is to
-                combine creativity with technical excellence to enhance the user
-                experience and drive business success.
-            </p>
+        <p className={`max-w-3xl ${alignment}`}>
+            Results-driven Front End Engineer with a strong passion for creating
+            visually appealing and highly responsive web applications.
+            Leveraging expertise in modern web technologies, I aim to contribute
+            to a dynamic team where I can design and develop user-friendly
+            interfaces, optimize website performance, and ensure seamless
+            cross-browser compatibility. My goal is to combine creativity with
+            technical excellence to enhance the user experience and drive
+            business success.
+        </p>
     );
 }
--- src/components/cards/AboutMe/AboutMeCard.tsx
+++ src/components/cards/AboutMe/AboutMeCard.tsx
@@ -13,16 +13,125 @@ import { Pages } from '../../../model/enum';
 import { useAppContext } from '../../../hooks/useAppContext';
 import { useGetData } from '../../../data/data';
 import Header from '../../Header';
+import { useBreakpoints } from '../../../hooks/useBreakpoints';
 import Summary from '../../Summary';
 
 export default function AboutMeCard() {
     const { setActivePage } = useAppContext();
     const { education } = useGetData();
+    const { isMobile, isTablet } = useBreakpoints();
+
+    const isSmall = isMobile || isTablet;
     const { jmu } = education;
 
     const handleClick = () => {
         setActivePage(Pages.HOME);
     };
+
+    const ProfileIcon = () => (
+        <Image
+            className='text-lg text-center'
+            alt='profile logo'
+            height={100}
+            radius='sm'
+            src='/profile.png'
+            width={100}
+        />
+    );
+
+    const LeftText = () => {
+        const alignment = isSmall ? 'items-center' : 'items-start';
+        return (
+            <div className={`flex grow ${alignment} flex-col`}>
+                <h2 className='text-lg text-start'>Christian Chenault</h2>
+                <h3 className='text-sm text-center text-default-500'>
+                    Front End Engineer
+                </h3>
+                <h3 className='text-sm text-center text-default-500'>
+                    Expertise: JavaScript, React, HTML, CSS
+                </h3>
+                <h3 className='text-sm text-start text-default-500'>
+                    Experience: 5+ years
+                </h3>
+            </div>
+        );
+    };
+
+    const RightText = () => {
+        const alignment = isSmall ? 'items-center' : 'items-end';
+        return (
+            <div className={`flex grow ${alignment} flex-col`}>
+                <div className='flex justify-between'>
+                    <div className='flex justify-end items-end'>
+                        <h3 className='text-sm text-end text-default-500'>
+                            <Link href='mailto:cchenault24@yahoo.com' size='sm'>
+                                cchenault24@yahoo.com
+                            </Link>
+                        </h3>
+                        <object
+                            className='mb-1 mx-2'
+                            data='/email.svg'
+                            width='18'
+                            height='18'
+                        />
+                    </div>
+                </div>
+                <div className='flex justify-between'>
+                    <div className='flex justify-end items-end'>
+                        <h3 className='text-sm text-end text-default-500'>
+                            <Link href='tel:+1804+291+8744' size='sm'>
+                                (804) 291-8744
+                            </Link>
+                        </h3>
+                        <object
+                            className='mb-1 mx-2'
+                            data='/phone.svg'
+                            width='18'
+                            height='18'
+                        />
+                    </div>
+                </div>
+                <div className='flex justify-between'>
+                    <div className='flex justify-end items-end'>
+                        <h3 className='text-sm text-end text-default-500'>
+                            <Link
+                                href='https://www.google.com/maps?q=Alexandria+VA'
+                                size='sm'
+                            >
+                                Alexandria, VA
+                            </Link>
+                        </h3>
+                        <object
+                            className='mb-1 mx-2'
+                            data='/location.svg'
+                            width='18'
+                            height='18'
+                        />
+                    </div>
+                </div>
+                <div className='flex justify-between'>
+                    <div className='flex justify-end items-end'>
+                        <h3 className='text-sm text-end text-default-500'>
+                            <Link
+                                href='https://www.linkedin.com/in/christianchenault/'
+                                size='sm'
+                            >
+                                LinkedIn
+                            </Link>
+                        </h3>
+                        <object
+                            className='mb-1 mx-2'
+                            data='/linkedin.svg'
+                            width='18'
+                            height='18'
+                        />
+                    </div>
+                </div>
+            </div>
+        );
+    };
+
+    const direction = isSmall ? 'flex-col' : 'flex-row'
 
     return (
         <>
@@ -36,105 +145,21 @@ export default function AboutMeCard() {
                 shadow='lg'
                 onClick={handleClick}
             >
-                <CardHeader className='flex gap-3'>
-                    <Image
-                        alt='profile logo'
-                        height={100}
-                        radius='sm'
-                        src='/profile.png'
-                        width={100}
-                    />
-                    <div className='flex grow items-start flex-col'>
-                        <div className='flex w-full justify-between'>
-                            <h2 className='w-fit text-lg text-start'>
-                                Christian Chenault
-                            </h2>
-                            <Spacer x={16} />
-                            <div className='flex w-fit justify-end items-end'>
-                                <h3 className='w-full text-sm text-end text-default-500'>
-                                    <Link
-                                        href='mailto:cchenault24@yahoo.com'
-                                        size='sm'
-                                    >
-                                        cchenault24@yahoo.com
-                                    </Link>
-                                </h3>
-                                <object
-                                    className='mb-1 mx-2'
-                                    data='/phone.svg'
-                                    width='18'
-                                    height='18'
-                                />
-                            </div>
-                        </div>
-                        <div className='flex w-full justify-between'>
-                            <h3 className='text-md w-fit text-start text-default-500'>
-                                Front End Engineer
-                            </h3>
-                            <Spacer x={16} />
-                            <div className='flex w-fit justify-end items-end'>
-                                <h3 className='w-full text-sm text-end text-default-500'>
-                                    <Link href='tel:+1804+291+8744' size='sm'>
-                                        (804) 291-8744
-                                    </Link>
-                                </h3>
-                                <object
-                                    className='mb-1 mx-2'
-                                    data='/phone.svg'
-                                    width='18'
-                                    height='18'
-                                />
-                            </div>
-                        </div>
-                        <div className='flex w-full justify-between'>
-                            <h3 className='text-md w-fit text-start text-default-500'>
-                                Expertise: JavaScript, React, HTML, CSS
-                            </h3>
-                            <Spacer x={16} />
-                            <div className='flex w-fit justify-end items-end'>
-                                <h3 className='w-full text-sm text-end text-default-500'>
-                                    <Link
-                                        href='https://www.google.com/maps?q=Alexandria+VA'
-                                        size='sm'
-                                    >
-                                        Alexandria, VA
-                                    </Link>
-                                </h3>
-                                <object
-                                    className='mb-1 mx-2'
-                                    data='/location.svg'
-                                    width='18'
-                                    height='18'
-                                />
-                            </div>
-                        </div>
-                        <div className='flex w-full justify-between'>
-                            <h3 className='text-md w-fit text-start text-default-500'>
-                                Experience: 5+ years
-                            </h3>
-                            <Spacer x={16} />
-                            <div className='flex w-fit justify-end items-end'>
-                                <h3 className='text-sm text-end text-default-500'>
-                                    <Link
-                                        href='https://www.linkedin.com/in/christianchenault/'
-                                        size='sm'
-                                    >
-                                        https://www.linkedin.com/in/christianchenault/
-                                    </Link>
-                                </h3>
-                                <object
-                                    className='mb-1 mx-2'
-                                    data='/linkedin.svg'
-                                    width='18'
-                                    height='18'
-                                />
-                            </div>
-                        </div>
-                    </div>
+
+                <CardHeader className={`flex gap-3 ${direction}`}>
+                    <ProfileIcon />
+                    <LeftText />
+                    {!isSmall && <RightText />}
                 </CardHeader>
                 <Divider />
                 <CardBody>
                     <Summary />
+                    {isSmall && (
+                        <>
+                            <Divider className='my-3'/>
+                            <RightText />
+                        </>
+                    )}
                 </CardBody>
                 <Divider />
                 <CardFooter className='flex justify-center'>
