231aaaad184902149995d69cc681b4983e4eaee0
Christian Chenault <cchenault24@yahoo.com>
2025-02-07 11:04:37 -0500

    Upgrade Tailwind CSS to v4 and update project configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,16 @@
 // tailwind.config.js
-import { nextui } from '@nextui-org/react';
+const { nextui } = require("@nextui-org/react");
 
 /** @type {import('tailwindcss').Config} */
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
-export const content = [
-    // ...
-    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
+module.exports = {
+  content: [
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-];
-export const theme = {
+  ],
+  theme: {
     extend: {},
+  },
+  darkMode: "class",
+  plugins: [nextui()],
 };
-export const darkMode = 'class';
-export const plugins = [nextui()];
